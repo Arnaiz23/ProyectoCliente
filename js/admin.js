bb3e230e0199782887efa0a874d58f82334920e5
Arnaiz23 <aaf110501@gmail.com>
2021-11-12 10:45:01 +0100

    Admin en proceso

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,33 @@
 document.getElementById("adminProductos").addEventListener("click",async ()=>{
+    let texto = document.querySelector(".main_informacion_derecha h3");
+    let container = document.querySelector(".main_informacion_derecha");
+    container.removeChild(texto);
+    container.insertAdjacentHTML("afterbegin",`
+        <h3>Productos</h3>
+        <table class="tabla">
+        </table>
+    `);
     let tabla = document.querySelector(".tabla");
     await getInfo().then(listas =>{
+        tabla.insertAdjacentHTML("afterbegin",`
+                <tr>
+                    <td>Imagen</td>
+                    <td>Nombre</td>
+                    <td>Breve descripcion</td>
+                    <td>Descripcion larga</td>
+                    <td>Precio</td>
+                </tr>
+        `);
+        tabla = document.querySelector(".tabla tr");
         listas.forEach(productos => {
             productos.forEach(producto => {
-                tabla.insertAdjacentHTML("afterbegin",`
+                tabla.insertAdjacentHTML("afterend",`
                     <tr>
                         <td><img src="${producto.imagen}" style="width:20%"></td>
                         <td>${producto.nombre}</td>
                         <td>${producto.descripcionCorta}</td>
                         <td>${producto.descripcion}</td>
-                        <td>${producto.precio}</td>
+                        <td>${producto.precio} €</td>
                     </tr>
                 `); 
             });
